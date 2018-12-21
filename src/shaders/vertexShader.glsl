00767a9acc928d71254dd693fc55c35075cdac5a
tana <yozoranotakaku@gmai.com>
2018-12-21 19:08:03 +0900

    fix: considering alpha

--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -3,5 +3,6 @@ varying vec4 v_color;
 
 void main() {
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    v_color = gl_Position * 0.5 + 0.5 + penColor;
+    v_color.rgb = gl_Position.xyz * 0.5 + 0.5 + penColor.rgb * penColor.a;
+    v_color.a   = 1.0;
 }
