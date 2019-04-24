4e50e83c5c96f17bafdada146e12f634f1f7e06f
tana <yozoranotakaku@gmail.com>
2019-04-24 12:02:53 +0900

    config: postcss-normalizeの導入

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,7 @@
 
 module.exports = {
     plugins: [
-        require('autoprefixer')({
-            'browsers': [
-                '> 0.5%',
-                'last 2 version',
-                'not dead'
-            ]
-        })
+        require('autoprefixer')(),
+        require('postcss-normalize')()
     ]
 }
